85d0bbad08a1e1801858aa3b96f63311d2242bed
brouwilliamog <william.pereira@focomultimidia.com>
2023-02-27 22:31:56 -0300

    Nova alterações

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    centeredSlides: true,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
