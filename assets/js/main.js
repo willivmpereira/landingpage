731c42e20020fbc1d10b6fd413093ff41c1cbbe1
brouwilliamog <william.pereira@focomultimidia.com>
2023-03-04 18:49:38 -0300

    SEO

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,7 +134,7 @@ const modal = {
   }
 }
 
-const buttons = document.querySelectorAll(".button__ask");
+const buttons = document.querySelectorAll(".open__ask");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () =>
